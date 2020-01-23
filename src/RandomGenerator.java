558b0984abf5ac791230e2532d9664ace7ca6415
= <austinzhwang@gmail.com>
2020-01-23 11:11:31 -0500

    random debugging

---
+++ src/RandomGenerator.java
@@ -0,0 +1,20 @@
+import java.util.Random;
+
+public class RandomGenerator {
+    public static void main(String[] args)
+    {
+        output("Generate 10 random integers between 0 and 6");
+        Random rnd = new Random();
+
+        for (int i = 0; i < 10; i++) {
+            int randomInt = rnd.nextInt(6);
+            output("Generated number: " + randomInt);
+        }
+        output("Done.");
+    }
+
+    private static void output(String aMessage)
+    {
+        System.out.println(aMessage);
+    }
+}
