b58167c113d1536dbf74fbb61177066da3215dff
= <austinzhwang@gmail.com>
2020-01-23 11:49:27 -0500

    escape sequences ii

---
+++ src/EscapeSequencesII.java
@@ -0,0 +1,10 @@
+public class EscapeSequencesII {
+    public static void main (String []args) {
+
+        System.out.print("Cost\tQuantity\tTotal\n" +
+                "$1,000.00\t4\t$4,000.00\n" +
+                "$\t50.00\t8\t  $400.00\n" +
+                "___________________________\n" +
+                "TOTAL:\t\t12\t$4,400.00");
+    }
+}
